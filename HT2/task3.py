c78098c0b7da4c4505f775283071600ed7a5cf04
OleksandrBurdelo <64668456+oleksandrburdelo@users.noreply.github.com>
2021-07-13 16:21:38 +0300

    Delete HT2 directory

--- HT2/task3.py
+++
@@ -1,8 +0,0 @@
-vegans = ['Oleksandr', 'Daryna', 'Denys']
-meat_and_vegetable_lovers = ['Petro', 'Ganna', 'Serhii']
-
-people = [vegans, meat_and_vegetable_lovers]
-
-for index in people:
-    for index2 in index:
-        print(index2)
