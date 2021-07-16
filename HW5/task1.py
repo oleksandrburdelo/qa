49bf4c572f458e233caa6f30ce5288a9714d58b4
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-07-16 21:36:06 +0300

    HW5: add comments with described issues and alternative solutions

--- HW5/task1.py
+++ HW5/task1.py
@@ -3,7 +3,7 @@ import os
 import pickle
 
 some_file = 'text.txt'
-numbers = [tuple(result for result in [randint(1,1000), randint(1,1000), randint(1,3)]) for row in range(100)]
+numbers = [tuple(result for result in [randint(1,1000), randint(1,1000), randint(1,3)]) for row in range(100)]  # too much overhead=)
 
 os.makedirs('./test/test1')
 os.chdir('test/test1')
@@ -12,3 +12,19 @@ os.system(f"touch {some_file}")
 with open(some_file, 'wb') as file:
     data = pickle.dumps(numbers)
     file.write(data)
+
+# Take a look how it could be written cleaner
+# import pickle
+# import os
+#
+# from random import randint
+#
+#
+# os.makedirs("test/data")
+# operations = []
+#
+# for _ in range(100):
+#     operations.append((randint(1, 100), randint(1, 100), randint(1, 3)))
+#
+# with open("test/data/text.txt", "wb") as file:
+#     pickle.dump(operations, file)
--- HW5/task2.py
+++ HW5/task2.py
@@ -6,3 +6,20 @@ with open('./test/test1/text.txt', 'rb') as file:
 
 for text3 in text2:
     print(text3)
+
+# good but you should print each operation
+# Also not clear names of variables take a look how it could be solved cleaner
+# Not bad but it could be solved in more elegant way
+# import pickle
+#
+# with open('test/data/text.txt', "rb") as file:
+#     operations = pickle.load(file)
+#
+# for operation in operations:
+#     left, right, operator = operation
+#     if operator == 1:
+#         print(f"{left} + {right} = {left + right}")
+#     elif operator == 2:
+#         print(f"{left} * {right} = {left * right}")
+#     else:
+#         print(f"{left} / {right} = {left / right}")
