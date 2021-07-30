6bb19b1e5b5f89d106d0384f3bfb95c350c0792c
oleksandrburdelo <burdelo.oleksandr@gmail.com>
2021-07-30 03:21:46 +0300

    HW8: all tasks

---
+++ HW8/task1.py
@@ -0,0 +1,27 @@
+def mathoperation(func):
+    def inner(*args, **kwargs):
+        func_name = func.__name__
+        func_res = func(*args, **kwargs)
+        print(func_name)
+        return func_res
+    return inner
+
+@mathoperation
+def Multiplication(number_one: {int}, number_two: {int}) -> {int}:
+    result = number_one * number_two
+    return result
+
+@mathoperation
+def Division(number_one: {int}, number_two: {int}) -> {int}:
+    result = number_one // number_two
+    return result
+
+@mathoperation
+def Int_Division(number_one: {int}, number_two: {int}) -> {int}:
+    result = number_one / number_two
+    return result
+
+if __name__ == '__main__':
+    print(Multiplication(10, 4))
+    print(Division(5, 3))
+    print(Int_Division(25, 11))
