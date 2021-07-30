51ee456a3859a776293b1ba3b82b244e385859ec
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-07-30 18:09:54 +0300

    HW8: add comments with described issues for task 2

--- HW8/task1.py
+++ HW8/task1.py
@@ -6,22 +6,27 @@ def mathoperation(func):
         return func_res
     return inner
 
+
 @mathoperation
 def Multiplication(number_one: {int}, number_two: {int}) -> {int}:
     result = number_one * number_two
     return result
+
 
 @mathoperation
 def Division(number_one: {int}, number_two: {int}) -> {int}:
     result = number_one // number_two
     return result
 
+
 @mathoperation
 def Int_Division(number_one: {int}, number_two: {int}) -> {int}:
     result = number_one / number_two
     return result
 
+
 if __name__ == '__main__':
-    print(Multiplication(10, 4))
+    print(5 + Multiplication(10, 4))
     print(Division(5, 3))
     print(Int_Division(25, 11))
+    # Good. Names of functionsa and variables should be in lower case and snake case
--- HW8/task2.py
+++ HW8/task2.py
@@ -2,5 +2,9 @@ def func_square():
     square = ((item ** 2 for item in range(0, 1000000000) if item % 2 == 0))
     return square
 
+
 for square1 in func_square():
     print(square1)
+
+# Welll it will work but I want to see generator comprehension or generator function
+# 2 points
