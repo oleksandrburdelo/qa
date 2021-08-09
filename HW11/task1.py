ed37a956df321b2b24549d5e72d1b3da22f65d82
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-08-09 16:07:16 +0300

    HW11: described issues in comments

--- HW11/task1.py
+++ HW11/task1.py
@@ -1,7 +1,9 @@
 from abc import ABC
 
+
 class Foundation_of_Car(ABC):
     def __int__(self):
+        # region TODO: this fields could be protected to share only with child
         self.wheels = 4
         self.engine = True
         self.body_type = True
@@ -9,7 +11,9 @@ class Foundation_of_Car(ABC):
         self.fuel_tank = True
         self.headlights = True
         self.frontlights = True
+        # endregion
 
+    # region TODO: incorrect usage of static methods
     @staticmethod
     def moving_streight(self):
         print("Moving forward")
@@ -29,7 +33,9 @@ class Foundation_of_Car(ABC):
     @staticmethod
     def stop(self):
         print("stop")
+    # endregion
 
+    # region TODO: better to use abstract method decorator
     def engine_work(self):
         raise NotImplementedError("not implemented")
 
@@ -38,16 +44,19 @@ class Foundation_of_Car(ABC):
 
     def refuel_car(self):
         raise NotImplementedError("not implemented")
+    # endregion
 
 
 class DieselCar(Foundation_of_Car):
     def __init__(self):
         super().__init__()
+        # region TODO: this fields should be protected
         self.wheels = 4
         self.body_type = "Wagon or Sedan"
         self.engine = "Diesel"
         self.fuel_economy = "l/100 km"
         self.exhaust_system = True
+        # endregion
 
     def acceleration_diesel(self):
         print("acceleration from 0 to 50 km/h")
@@ -56,40 +65,45 @@ class DieselCar(Foundation_of_Car):
         print("car fueling on gas station")
 
 
-
 class PetrolCar(Foundation_of_Car):
     def __init__(self):
         super().__init__()
+        # region TODO: this fields should be protected
         self.wheels = 4
         self.body_type = "Wagon or Sedan"
         self.engine = "Petrol"
         self.exhaust_system = True
-
+        # endregion
 
     def acceleration_petrol(self):
         print("acceleration from 0 to 50 km/h")
-
 
     def refuel_tank_petrol(self):
         print("car fueling on gas station")
 
 
-
 class MB_C_Class(DieselCar):
     def __init__(self, model: str, body_type: str, engine_type: str, fuel_economy: str, doors: str):
+        # TODO: constructor of Dielsel car does not used so all fields declired iinside Dielsel car missed
+        # region TODO: this fields could be private since not used outside of class
         self.model = model
         self.body_type = body_type
         self.engine_type = engine_type
         self.fuel_economy = fuel_economy
         self.doors = doors
+        # endregion
+
 
 class Ford_Mustang(PetrolCar):
     def __init__(self, model: str, body_type: str, engine_type: str, fuel_economy: str, doors: str):
+        # TODO: constructor of petroll does not used so all fields declired iinside petroll missed
+        # region TODO: this fields could be private since not used outside of class
         self.model = model
         self.body_type = body_type
         self.engine_type = engine_type
         self.fuel_economy = fuel_economy
         self.doors = doors
+        # endregion
 
 
 if __name__ == '__main__':
